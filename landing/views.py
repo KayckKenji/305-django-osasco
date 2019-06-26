df9bc8e49bd3e1599b60f759b1a596164c7e514f
Kenji <51339721+KayckKenji@users.noreply.github.com>
2019-06-26 16:15:25 -0300

    testes com django - osasco, if, else, etc

---
+++ landing/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def mostrar_index(request):
+        return render(request, 'index.html')
+def mostrar_pombos(request):
+        return render(request, 'pombos.html')
+def mostrar_rolezinho(request):
+        roles = ['bailao', 'Shopping União', 'Quermesse da Paróquia de São João', 'Calçadão de Osasco', 'SESC', 'Risca Faca' ]
+        bairro = 'Rochdale'
+        return render(request, 'rolezinho.html', {'roles':roles, 'bairro':bairro})
